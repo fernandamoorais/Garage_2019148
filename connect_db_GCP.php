687f2c81575f15de1125044194148992dd7636e9
Fernanda Gomes <fernandamoorais@hotmail.com>
2020-07-07 14:42:24 +0100

    Using code from CA2 Web development

---
+++ connect_db_GCP.php
@@ -0,0 +1,31 @@
+<?php
+//My previous code from CA2 - web develpment. 
+//Need to be edited to ger garages DB
+// Credentials
+$dbhost = '127.0.0.1:3306';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'user_db';
+
+
+//Only for online 
+/*
+$dbhost = 'sql311.epizy.com';
+$dbuser = 'epiz_24967007';
+$dbpass = '12Fernanda34';
+$dbname = 'epiz_24967007_user_db';
+*/
+
+
+// 1. Create a database connection
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+// Check connection
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+//echo "Connected successfully";
+
+// 5. Close database connection
+//mysqli_close($connection);
+?>
