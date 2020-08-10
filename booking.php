d15c4dbf3d8df9253398553b9d88a6f36b2dd5c6
Fernanda Gomes <fernandamoorais@hotmail.com>
2020-08-10 21:01:33 +0100

    PHP

---
+++ booking.php
@@ -0,0 +1,7 @@
+//the the user sucessfully login in he is foward tothis page. 
+
+/*if (!isLoggedIn()) {
+$_SESSION['msg'] = "You must log in first";
+header('location: login.php');
+}*/
+//if some1 try to acess yhis index without logging it will foward the user to the login.php page
